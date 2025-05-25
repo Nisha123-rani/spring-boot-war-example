6b6f91aab724f14889d995e8b075ebf81ced7157
Nisha123-rani <nisha.rani27061988@gmail.com>
2025-05-26 00:25:20 +0530

    Update HelloWorldApplication.java

--- src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
@@ -23,7 +23,7 @@ public class HelloWorldApplication extends SpringBootServletInitializer {
 
     @RequestMapping("/")
     String helloWorld() {
-        return "Hello nisha rani!";
+        return "Hello sarvagya!";
     }
 
 }
